9f022bac564c0d4cfcba9d4ca116929531c92a09
aleareyatol <aleareyatol@gmail.com>
2026-03-11 15:56:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,36 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# sample data
+student = {
+    "name": "Your Name",
+    "grade": 10,
+    "section": "Zechariah"
+}
+
 @app.route('/')
 def home():
     return "Welcome to my Flask API!"
 
-@app.route('/student')
+# GET student
+@app.route('/student', methods=['GET'])
 def get_student():
+    return jsonify(student)
+
+# UPDATE student
+@app.route('/student', methods=['PUT'])
+def update_student():
+    data = request.get_json()
+
+    student["name"] = data.get("name", student["name"])
+    student["grade"] = data.get("grade", student["grade"])
+    student["section"] = data.get("section", student["section"])
+
     return jsonify({
-        "name": "Your Name",
-        "grade": 10,
-        "section": "Zechariah"
+        "message": "Student updated successfully",
+        "student": student
     })
 
 if __name__ == "__main__":
     app.run(debug=True)
+
